77c7f5f44ead68ae467ab1ec59ffd3fe61bd5ec7
kphed <kpcrypto69@gmail.com>
2023-10-18 13:30:18 -0400

    Update file and contract names

--- script/BRR.s.sol
+++ script/BRR.s.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract BRRScript is Script {
     function setUp() public {}
 
     function run() public {
---
+++ test/BRR.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "src/BRR.sol";
+
+contract BRRTest is Test {}
