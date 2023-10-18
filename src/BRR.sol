34f94115437bbf4d243bf08c0cc47eb5f23171bf
kphed <kpcrypto69@gmail.com>
2023-10-18 13:32:21 -0400

    Inherit from Solady ERC20 and implement name and symbol

--- src/BRR.sol
+++ src/BRR.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract BRR {}
+import {ERC20} from "solady/tokens/ERC20.sol";
+
+contract BRR is ERC20 {
+    function name() public pure override returns (string memory) {
+        return "BRR (Brrito.xyz)";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "BRR";
+    }
+}
