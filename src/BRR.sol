79d4cfb16b58d16f4c717eefdbe91ea68b07ef5d
kphed <kpcrypto69@gmail.com>
2023-10-18 13:51:17 -0400

    Configure Ownable and add maxSupply setter

--- src/BRR.sol
+++ src/BRR.sol
@@ -1,14 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {Ownable} from "solady/auth/Ownable.sol";
 import {ERC20} from "solady/tokens/ERC20.sol";
 
-contract BRR is ERC20 {
+contract BRR is Ownable, ERC20 {
+    uint256 public maxSupply = 1e9;
+
+    event SetMaxSupply(uint256 newMaxSupply);
+
+    error CannotIncreaseMaxSupply();
+    error MaxSupplyTooLow();
+
+    constructor(address initialOwner) {
+        _initializeOwner(initialOwner);
+    }
+
     function name() public pure override returns (string memory) {
         return "BRR (Brrito.xyz)";
     }
 
     function symbol() public pure override returns (string memory) {
         return "BRR";
+    }
+
+    function setMaxSupply(uint256 newMaxSupply) external onlyOwner {
+        // We are only allowing the max supply to be set to a lower value as a
+        // commitment to the community that we won't mint more tokens than needed.
+        if (newMaxSupply > maxSupply) revert CannotIncreaseMaxSupply();
+
+        if (newMaxSupply < totalSupply()) revert MaxSupplyTooLow();
+
+        maxSupply = newMaxSupply;
+
+        emit SetMaxSupply(newMaxSupply);
     }
 }
--- test/BRR.t.sol
+++ test/BRR.t.sol
@@ -2,6 +2,72 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
-import "src/BRR.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
+import {BRR} from "src/BRR.sol";
 
-contract BRRTest is Test {}
+contract BRRTest is Test {
+    bytes32 private constant _TOTAL_SUPPLY_SLOT =
+        bytes32(uint256(0x05345cdf77eb68f44c));
+
+    BRR public immutable token = new BRR(address(this));
+
+    event SetMaxSupply(uint256 newMaxSupply);
+
+    /*//////////////////////////////////////////////////////////////
+                             setMaxSupply
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotSetMaxSupplyUnauthorized() external {
+        address msgSender = address(0);
+        uint256 newMaxSupply = 0;
+
+        assertTrue(msgSender != token.owner());
+
+        vm.prank(msgSender);
+        vm.expectRevert(Ownable.Unauthorized.selector);
+
+        token.setMaxSupply(newMaxSupply);
+    }
+
+    function testCannotSetMaxSupplyCannotIncreaseMaxSupply() external {
+        address msgSender = token.owner();
+        uint256 newMaxSupply = token.maxSupply() + 1;
+
+        vm.prank(msgSender);
+        vm.expectRevert(BRR.CannotIncreaseMaxSupply.selector);
+
+        token.setMaxSupply(newMaxSupply);
+    }
+
+    function testCannotSetMaxSupplyMaxSupplyTooLow() external {
+        uint256 totalSupply = 1;
+
+        vm.store(address(token), _TOTAL_SUPPLY_SLOT, bytes32(totalSupply));
+
+        assertEq(totalSupply, token.totalSupply());
+
+        address msgSender = token.owner();
+        uint256 newMaxSupply = totalSupply - 1;
+
+        vm.prank(msgSender);
+        vm.expectRevert(BRR.MaxSupplyTooLow.selector);
+
+        token.setMaxSupply(newMaxSupply);
+    }
+
+    function testSetMaxSupply() external {
+        address msgSender = token.owner();
+        uint256 newMaxSupply = token.maxSupply() - 1;
+
+        assertTrue(newMaxSupply != token.maxSupply());
+
+        vm.prank(msgSender);
+        vm.expectEmit(false, false, false, true, address(token));
+
+        emit SetMaxSupply(newMaxSupply);
+
+        token.setMaxSupply(newMaxSupply);
+
+        assertEq(newMaxSupply, token.maxSupply());
+    }
+}
